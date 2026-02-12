c1960b1d31c4471c7c849ccf0775fb9c520d2884
rohitankolnerkar <rohit.ankolnerkar@gammaedge.io>
2026-02-12 11:45:21 +0530

    testci/cd12345

--- app/core/security.py
+++ app/core/security.py
@@ -13,9 +13,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_async_db
 from app.models.users import User
 
+SECRET_KEY = os.getenv("SECRET_KEY", "CHANGE_ME_IN_PRODUCTION")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
 
 
 pwd_context = CryptContext(

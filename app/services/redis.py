76fb3f4f96c19e1874da303ac2773c4b01c3cbd5
rohitankolnerkar <rohit.ankolnerkar@gammaedge.io>
2026-02-13 13:17:01 +0530

    env problem solved

--- app/services/redis.py
+++ app/services/redis.py
@@ -1,15 +1,13 @@
 import os
 
-from dotenv import load_dotenv
 from redis.asyncio import Redis
 
-load_dotenv()
-
 REDIS_URL = os.getenv("REDIS_URL")
-if not REDIS_URL:
-    raise RuntimeError("REDIS_URL not found in environment")
 
-redis_client = Redis.from_url(
-    REDIS_URL,
-    decode_responses=True,
-)
+redis_client: Redis | None = None
+
+if REDIS_URL:
+    redis_client = Redis.from_url(
+        REDIS_URL,
+        decode_responses=True,
+    )

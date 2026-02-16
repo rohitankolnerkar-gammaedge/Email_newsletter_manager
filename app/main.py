685fad91346f2c7084d29921ddace79121679a4f
rohitankolnerkar <rohit.ankolnerkar@gammaedge.io>
2026-02-16 17:05:30 +0530

    git deploy

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import os
+
+import uvicorn
 from fastapi import FastAPI
 
 from app.api.api import router
@@ -6,20 +9,31 @@ from app.db.session import engine
 
 app = FastAPI()
 
+# Include routers
+app.include_router(router, prefix="/api")
 
+
+# Create database tables
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 
-app.include_router(router, prefix="/api")
-
-
+# Startup event
 @app.on_event("startup")
 async def on_startup():
     await create_tables()
 
 
+# Root endpoint
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
+
+
+# Only run with Python, not when imported
+if __name__ == "__main__":
+    port = int(
+        os.environ.get("PORT", 8000)
+    )  # use PORT from env or default 8000 locally
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
